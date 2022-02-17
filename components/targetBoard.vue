4c87787e099ed8f63c4fb3963e613554681f18ec
Danni <dannielle.young@testdrivensolutions.co.uk>
2022-02-17 16:17:46 +0000

    added counting of thows and the inability to click 7 until the last round

--- components/targetBoard.vue
+++ components/targetBoard.vue
@@ -1,35 +1,40 @@
 <template>
-  <div class="w-1/2">
-    <div class="flex flex-row">
-      <div
-        class="border-2 border-green-700 rounded-full h-1/2 w-7"
-        @click.stop="onClick(7)"
-      >
-        <p class="ml-2">7</p>
-      </div>
-      <div
-        class="mt-5 border-2 border-blue-700 rounded-full w-32"
-        @click.stop="onClick(1)"
-      >
-        <p class="ml-14">1</p>
+  <div>
+    <p>Round Score: {{ roundScore }}</p>
+    <p>Throw Count: {{ throwCount }}</p>
+
+    <div class="w-52 border">
+      <div class="flex flex-row p-3">
         <div
-          class="border-2 border-black rounded-full w-20 m-auto mb-7"
-          @click.stop="onClick(3)"
+          class="border-2 border-green-700 rounded-full h-1/2 w-7"
+          @click.stop="onClick(7)"
         >
-          <p class="ml-8">3</p>
+          <p class="ml-2">7</p>
+        </div>
+        <div
+          class="mt-5 border-2 border-blue-700 rounded-full w-32"
+          @click.stop="onClick(1)"
+        >
+          <p class="ml-14">1</p>
           <div
-            class="border-2 border-red-700 rounded-full w-7 m-auto mb-7"
-            @click.stop="onClick(5)"
+            class="border-2 border-black rounded-full w-20 m-auto mb-7"
+            @click.stop="onClick(3)"
           >
-            <p class="ml-2">5</p>
+            <p class="ml-8">3</p>
+            <div
+              class="border-2 border-red-700 rounded-full w-7 m-auto mb-7"
+              @click.stop="onClick(5)"
+            >
+              <p class="ml-2">5</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        class="border-2 border-green-700 rounded-full h-1/2 w-7"
-        @click.stop="onClick(7)"
-      >
-        <p class="ml-2">7</p>
+        <div
+          class="border-2 border-green-700 rounded-full h-1/2 w-7"
+          @click.stop="onClick(7)"
+        >
+          <p class="ml-2">7</p>
+        </div>
       </div>
     </div>
   </div>
@@ -37,9 +42,22 @@
 
 <script>
 export default {
+  data() {
+    return {
+      roundScore: 0,
+      throwCount: 0,
+    }
+  },
   methods: {
     onClick(number) {
-      console.log(number)
+      //7 is only available on the final throw
+      if (this.throwCount < 4 && number !== 7) {
+        this.roundScore += number
+        this.throwCount += 1
+      } else if (this.throwCount === 4) {
+        this.roundScore += number
+        this.throwCount += 1
+      }
     },
   },
 }
