434d2fd5ffb7cab6304b2bc6a0dc36decbb36752
danni-young-tds <dannielle.young@testdrivensolutions.co.uk>
2022-08-02 17:16:10 +0100

    added game over page to link

---
+++ pages/game-over.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>The final scores are in...</h1>
+    //LIST HERE THE SCORES AND WHO WINNER
+    //LINK FOR A NEW GAME
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
