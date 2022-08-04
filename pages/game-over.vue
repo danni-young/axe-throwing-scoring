9985368e8f37df4eea9d49e44975739e75845657
danni-young-tds <dannielle.young@testdrivensolutions.co.uk>
2022-08-04 17:18:39 +0100

    added list of players in score order

--- pages/game-over.vue
+++ pages/game-over.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
     <h1>The final scores are in...</h1>
-    //LIST HERE THE SCORES AND WHO WINNER
-    //LINK FOR A NEW GAME
+    <p v-for="player in scores.results">{{player.name}} -> {{player.score}}</p>
+    <nuxt-link to="/playerDetails" class="border"
+      >Play again?</nuxt-link>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage',
-})
+<script>
+import {calculateHighestScore} from '../lib/calculateHighestScore'
+export default {
+  name: 'game-over',
+  methods: {
+    clearStore: {
+      //create a default state and reset state with this to clear
+      // then we can route to the player page
+    }
+  },
+  computed: {
+    scores(){
+      return calculateHighestScore(this.$store.state.players.scores)
+    }
+  }
+}
 </script>
