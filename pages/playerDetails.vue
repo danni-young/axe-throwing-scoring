cc50bf31c3aca6fb27cd2304d04607fe01f561bd
danni-young <99653990+danni-young@users.noreply.github.com>
2022-11-15 22:50:36 +0000

    added style to the players page

--- pages/playerDetails.vue
+++ pages/playerDetails.vue
@@ -1,18 +1,20 @@
 <template>
-  <div>
-    <p>Enter player names:</p>
-    <p :v-if="error.length > 0" class="font-bold text-red-500">{{ error }}</p>
-    <div class="flex flex-col w-1/2">
-      <input
-        v-for="player in players"
-        :key="player"
-        class="border mb-2 ml-3"
-        :placeholder="player"
-        v-model="$data[player]"
-      />
-      <button v-on:click="addPlayers([player1, player2, player3, player4])">
-        Save players
-      </button>
+  <div class="bg-green-400 w-screen h-screen flex flex-row items-center">
+    <div class="w-1/2 mx-auto flex flex-col items-center">
+      <p class="text-gray-800 uppercase text-3xl font-bold mt-6 mb-3">Enter player names</p>
+      <p :v-if="error.length > 0" class="font-bold text-red-500">{{ error }}</p>
+      <div class="flex flex-col w-1/2">
+        <input
+          v-for="(player, index) in players"
+          :key="player"
+          class="border mb-2 ml-3"
+          :placeholder="` Player ${index + 1}`"
+          v-model="$data[player]"
+        />
+        <button v-on:click="addPlayers([player1, player2, player3, player4])" class="bg-white hover:bg-gray-100 text-gray-800 lg:text-base text-sm font-semibold py-2 px-4 border border-gray-400 rounded shadow w-content mx-auto mt-2">
+          Save players
+        </button>
+      </div>
     </div>
   </div>
 </template>
