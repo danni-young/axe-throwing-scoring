7bf4b24826d679cdc093ac27f668fb252eba7309
danni-young <99653990+danni-young@users.noreply.github.com>
2023-01-17 16:53:27 +0000

    enabled return if no results and added class

--- lib/calculateHighestScore.ts
+++ lib/calculateHighestScore.ts
@@ -1,8 +1,25 @@
-export const calculateHighestScore = (scores: {
-  [index: string]: { rounds: number[]; total: number }
-}): { results: { name: string; score: number }[]; draw: boolean } => {
+export const calculateHighestScore = (scores: 
+  {
+     [index: string]: { 
+        rounds: number[]; 
+        total: number 
+      }
+}): { 
+  results: 
+    { 
+      name: string; 
+      score: number;
+      class: string;
+    }[]; 
+      draw: boolean 
+    } => {
   const playerNames: string[] = Object.keys(scores)
-
+if(playerNames.length === 0) {
+  return {
+    results: [],
+    draw: false
+  }
+}
   const results = playerNames
     .map((name: string) => {
       return {
@@ -11,6 +28,12 @@ export const calculateHighestScore = (scores: {
       }
     })
     .sort((a, b) => b.score - a.score)
+    .map((person, index) => {
+      return {
+        ...person,
+        class: index === 0 ? 'mb-2 bg-yellow-600 font-semibold' : 'mb-2 font-semibold'
+      }
+    })
   //TODO: return winner or winners, the runners up, won't need draw then
   const draw = results[0].score === results[1].score
   return { results, draw }
--- test/calculateHighestScore.test.ts
+++ test/calculateHighestScore.test.ts
@@ -9,8 +9,8 @@ describe('calculateHighestScore', () => {
       })
     ).toStrictEqual({
       results: [
-        { name: 'Matthew', score: 2 },
-        { name: 'Danni', score: 1 },
+        { name: 'Matthew', score: 2,  "class": "mb-2 bg-yellow-600 font-semibold" },
+        { name: 'Danni', score: 1,  "class": "mb-2 font-semibold"},
       ],
       draw: false,
     })
@@ -18,15 +18,27 @@ describe('calculateHighestScore', () => {
   it('should highlight a draw', () => {
     expect(
       calculateHighestScore({
-        Danni: { total: 1, rounds: [1] },
-        Matthew: { total: 1, rounds: [1] },
+        Danni: { total: 1, rounds: [1]},
+        Matthew: { total: 1, rounds: [1]},
       })
     ).toStrictEqual({
       results: [
-        { name: 'Danni', score: 1 },
-        { name: 'Matthew', score: 1 },
+        { name: 'Danni', score: 1,   "class": "mb-2 bg-yellow-600 font-semibold" },
+        { name: 'Matthew', score: 1 ,  "class": "mb-2 font-semibold"},
       ],
       draw: true,
+    })
+  })
+  it('should return empty objects to deal with page clicked on with no scores', () => {
+    expect(
+      calculateHighestScore({
+       
+      })
+    ).toStrictEqual({
+      results: [
+  
+      ],
+      draw: false,
     })
   })
 })
